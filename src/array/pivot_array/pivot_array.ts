25ad591b04d17935044f088a1053301226c3d6d9
Burhan <burhan19_93@yahoo.com>
2022-03-30 12:57:33 +0800

    rename variable

--- src/array/pivot_array/pivot_array.ts
+++ src/array/pivot_array/pivot_array.ts
@@ -1,17 +1,17 @@
 
 
-export function pivotIndex(nums: number[]): number {
+export function pivotIndex(arrayOfNumber: number[]): number {
 
     let leftSum = 0
     let rightSum = 0
-    const total = nums.reduce((a, b) => a + b)
-    for (let index = 0; index < nums.length; index++) {
-        rightSum = total - (leftSum + nums[index])
+    const total = arrayOfNumber.reduce((a, b) => a + b)
+    for (let index = 0; index < arrayOfNumber.length; index++) {
+        rightSum = total - (leftSum + arrayOfNumber[index])
         if (leftSum == rightSum) {
             return index
         }
 
-        leftSum += nums[index]
+        leftSum += arrayOfNumber[index]
     }
 
     return -1
