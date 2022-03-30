77ba38e3cfffa2fc03de521321cab1f0bc2b51db
Burhan <burhan19_93@yahoo.com>
2022-03-30 12:19:48 +0800

    updated test

--- src/array/pivot_array/pivot_array.test.ts
+++ src/array/pivot_array/pivot_array.test.ts
@@ -6,4 +6,14 @@ describe("pivotIndex", () => {
         const arrayOfNumber = [1, 7, 3, 6, 5, 6]
         expect(pivotIndex(arrayOfNumber)).toEqual(3)
     });
+
+    it("for [1,2,3] should be -1", () => {
+        const arrayOfNumber = [1, 2, 3]
+        expect(pivotIndex(arrayOfNumber)).toEqual(-1)
+    });
+
+    it("for [2,1,-1] should be 0", () => {
+        const arrayOfNumber = [2, 1, -1]
+        expect(pivotIndex(arrayOfNumber)).toEqual(0)
+    });
 })
