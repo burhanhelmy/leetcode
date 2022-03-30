0153aeb637fa1ada23eb956595b885d5b0780335
Burhan <burhan19_93@yahoo.com>
2022-03-30 11:15:34 +0800

    complete setup for typescript

---
+++ src/app.ts
@@ -0,0 +1,3 @@
+import { pivotIndex } from "./array/pivot_array/pivot_array";
+
+pivotIndex([1, 2, 3, 4])
---
+++ src/array/pivot_array/pivot_array.ts
@@ -0,0 +1,9 @@
+
+
+export function pivotIndex(nums: number[]): number {
+    console.log("wee");
+    setInterval(() => {
+        console.log(nums[1])
+    }, 1000)
+    return -1
+};
